4c567eae587f6c17580c1702ffa144493d9ec712
Emre Surmeli <s.emre.s.8@gmail.com>
2015-08-13 13:09:03 -0700

    navbar working

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Footer from './Footer';
+import Navbar from './Navbar';
 import {RouteHandler} from 'react-router';
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <Navbar />
         <RouteHandler />
+        <Footer />
       </div>
     );
   }
